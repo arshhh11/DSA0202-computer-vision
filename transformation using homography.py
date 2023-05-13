bf40e2a3ae9b0f0ed3f2fc22bcad13a33f1317c8
arshhh11 <113960199+arshhh11@users.noreply.github.com>
2023-05-13 12:33:20 +0530

    Update transformation using homography.py

--- transformation using homography.py
+++ transformation using homography.py
@@ -2,9 +2,9 @@ import cv2
 import numpy as np
  
 if _name_ == '_main_' :
-    im_src = cv2.imread("C:/Users/mohammed rafik m/OneDrive/Pictures\Saved Pictures/WhatsApp Image 2022-03-18 at 6.54.12 AM.jpeg")
+    im_src = cv2.imread("C:\Users\Asus-2022\Downloads\cv.jpg")
     pts_src = np.array([[141, 131], [480, 159], [493, 630],[64, 601]])
-    im_dst = cv2.imread("C:/Users/mohammed rafik m/OneDrive/Pictures\Saved Pictures/WhatsApp Image 2022-03-18 at 6.54.12 AM.jpeg")
+    im_dst = cv2.imread("C:\Users\Asus-2022\Downloads\cv.jpg")
     pts_dst = np.array([[318, 256],[534, 372],[316, 670],[73, 473]])
     h, status = cv2.findHomography(pts_src, pts_dst)
     im_out = cv2.warpPerspective(im_src, h, (im_dst.shape[1],im_dst.shape[0]))
