4e689ceaf793e97d292d270628ee618e57c4dbf4
arshhh11 <113960199+arshhh11@users.noreply.github.com>
2023-05-13 12:23:50 +0530

    Update 16.Edge_detection U canny.py

--- 16.Edge_detection U canny.py
+++ 16.Edge_detection U canny.py
@@ -1,7 +1,7 @@
 import cv2
 
 # Load the image
-img = cv2.imread('C:/Users/mohammed rafik m/OneDrive/Documents/computer vision/WhatsApp Image 2022-03-18 at 6.54.12 AM.jpeg', 0)
+img = cv2.imread("C:\Users\Asus-2022\Downloads\cv.jpg", 0)
 
 # Apply Canny edge detection
 edges = cv2.Canny(img, 100, 200)
